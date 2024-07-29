0d502cf257f0c17340d9149f41642713a2fbe76d
13jalda <j.aldacarro@gmail.com>
2024-07-29 12:07:39 +0200

    added post topics to post component

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ export function fetchRecentPosts() {
     return function(dispatch) {
         axios.get('https://swapi.dev/api/people/')
         .then(response => {
-            console.log(response.data.results);
+            console.log(response.data);
             dispatch({
                 type: SET_RECENT_POSTS,
                 payload: response.data.results
--- src/components/post.js
+++ src/components/post.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
 class Post extends Component {
-    render() {
+  
+  
+  renderTopics() {
+    let topics = this.props.films.map((topic, index) => {
+          return <span className="post-topic" key={index}>{topic}</span>
+    })
+    return topics;
+  }  
+  
+  render() {
         return (
-          <li>
-            {this.props.name}
+          <li className="recent-post">
+              <div className="recent-post__title">
+                  {this.props.name}
+              </div>
+              <div className="recent-post__topics">
+                  {this.renderTopics()}
+              </div>
           </li>
         );
     }
